244caea9e77bda3418cdbe70cc6efb176d0ac5f0
Satyaprakash <satyaasingh001@gmail.com>
2024-10-13 01:20:24 +0530

    add functionality of access token and refresh token

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema(
     {
@@ -47,5 +49,42 @@ const userSchema = new Schema(
         timestamps: true,
     }
 );
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("labPassword")) return next();
+
+    this.labPassword = await bcrypt.hash(this.labPassword, 10);
+    next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (labPassword) {
+    return await bcrypt.compare(labPassword, this.labPassword);
+};
+
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+            labEmail: this.labEmail,
+            labName: this.labName,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+        }
+    );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+        }
+    );
+};
 
 export const User = mongoose.model("User", userSchema);
